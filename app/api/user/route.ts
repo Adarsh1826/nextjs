dc1d6a42d6b82b38565bd61b97b6d5d1582aa75b
adarsh singh <adsredmi182615@gmail.com>
2025-04-06 10:23:32 +0530

    added fe and be

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,15 +1,30 @@
+import { log } from "console";
+import { NextResponse } from "next/server";
 import { use } from "react";
 
 // for get handler
+const obj = [{}]
 interface test{
     username:string,
     email:string
 }
-export function GET(){
+export function GET(req:NextResponse){
     //console.log(typeof data);
     return Response.json({
        username:"test", 
        email:"test"
     }satisfies test)
 }
+export async  function POST(req:NextResponse){
+    const body = await req.json()
+    const {name,pass} = body;
+    obj.push({
+        name,
+        pass
+    })
+    console.log(obj)
+    return Response.json({
+        "msg":"Done"
+    })
 
+}
