1e5ba2e57dfd171d98e9510bf048fa3bc03e36df
adarsh singh <adsredmi182615@gmail.com>
2025-04-23 09:30:15 +0530

    route

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-export default function({children}:{
-    children:React.ReactNode
-}){
-    return <div>
-        <div className="border -b p-1">20 off</div>
-        {children}
-    </div>
-}
